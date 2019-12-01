78f1e9f885e2a8751965037d828da024a672b480
Anteroxc <53874213+Anteroxc@users.noreply.github.com>
2019-12-01 18:27:49 +0400

    Update HomeWork4Part2.kt

--- HomeWork4Part2.kt
+++ HomeWork4Part2.kt
@@ -1,26 +1,32 @@
-open class Pet()
-
-open class Cat(name: String) : Pet()
-class KittenMale(name: String) : Cat(name)
-class KittenFemale(name: String) : Cat(name)
-
-fun main(args: Array<String>) {
-    val pet = Pet()
-    node("AnyPet") {
-
-        node("CatMother") {
-
-            node("Kitten1") {
-
-            }
-            node("Kitten2") {
-
-            }
-        }
+open class Pet(name: String) {
+    val children = mutableListOf<Pet>()
+    fun makeChild(name: String,childScope: Pet.() -> Unit): Pet {
+        val pet = Pet("MotherPet")
+        pet.childScope()
+        children.add(pet)
+        return pet
     }
 }
 
+open class Cat(val name: String) : Pet(name)
 
-fun node(name: String, function: () -> Unit) {
-    println(name)
+fun main(args: Array<String>) {
+
+    val motherPet = node("AnyPet") {
+        this.children.add(this)
+        this.makeChild("Kitty"){
+            this.children.add(this)
+            this.makeChild("evenMoreKitty"){
+                this.children.add(this)
+            }
+            this.makeChild("Kitty2"){
+                this.children.add(this)
+                this.makeChild("evenMoreKitty2"){
+                }
+            }
+            this.makeChild("Cat"){
+                this.children.add(this)
+            }
+        }
+    }
 }
